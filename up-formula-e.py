5e5222e4b3077a89d77f9220737049a034cd18d3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-30 22:07:30 -0300

    refatoracao em dict

--- up-formula-e.py
+++ up-formula-e.py
@@ -2,77 +2,78 @@ import random
 from os import system as sys
 
 quiz = [
-    [
-        "Qual é a principal fonte de energia utilizada pela Fórmula E?",
-        ["Combustível fóssil", "Eletricidade", "Biocombustível"],
-        1,
-    ],
-    [
-        "Como a Fórmula E contribui para a redução da emissão de gases de efeito estufa?",
-        [
+    {
+        "pergunta": "Qual é a principal fonte de energia utilizada pela Fórmula E?",
+        "alternativas": ["Combustível fóssil", "Eletricidade", "Biocombustível"],
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Como a Fórmula E contribui para a redução da emissão de gases de efeito estufa?",
+        "alternativas": [
             "Usando carros híbridos",
             "Utilizando motores elétricos",
             "Diminuindo o número de corridas",
         ],
-        1,
-    ],
-    [
-        "Em que tipo de local geralmente acontecem as corridas da Fórmula E?",
-        ["Autódromos permanentes", "Circuitos de rua", "Pistas de aeroportos"],
-        1,
-    ],
-    [
-        "Qual é uma vantagem ambiental direta das baterias usadas na Fórmula E?",
-        ["Produzem mais ruído", "Geram menos emissões", "São mais baratas"],
-        1,
-    ],
-    [
-        "Como a Fórmula E promove a sustentabilidade nas cidades onde realiza corridas?",
-        [
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Em que tipo de local geralmente acontecem as corridas da Fórmula E?",
+        "alternativas": ["Autódromos permanentes", "Circuitos de rua", "Pistas de aeroportos"],
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Qual é uma vantagem ambiental direta das baterias usadas na Fórmula E?",
+        "alternativas": ["Produzem mais ruído", "Geram menos emissões", "São mais baratas"],
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Como a Fórmula E promove a sustentabilidade nas cidades onde realiza corridas?",
+        "alternativas": [
             "Através de campanhas de sensibilização ambiental",
             "Dando ingressos gratuitos",
             "Construindo novas pistas permanentes",
         ],
-        0,
-    ],
-    [
-        "O que acontece com as baterias usadas pelos carros da Fórmula E após o uso?",
-        ["São descartadas em aterros", "São recicladas", "São vendidas como sucata"],
-        1,
-    ],
-    [
-        "Qual é o impacto do menor nível de ruído dos carros da Fórmula E nas cidades?",
-        ["Reduz a poluição sonora", "Aumenta a poluição sonora", "Não tem impacto"],
-        0,
-    ],
-    [
-        "Como a Fórmula E ajuda a promover a inovação em veículos elétricos?",
-        [
+        "resposta_idx": 0,
+    },
+    {
+        "pergunta": "O que acontece com as baterias usadas pelos carros da Fórmula E após o uso?",
+        "alternativas": ["São descartadas em aterros", "São recicladas", "São vendidas como sucata"],
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Qual é o impacto do menor nível de ruído dos carros da Fórmula E nas cidades?",
+        "alternativas": ["Reduz a poluição sonora", "Aumenta a poluição sonora", "Não tem impacto"],
+        "resposta_idx": 0,
+    },
+    {
+        "pergunta": "Como a Fórmula E ajuda a promover a inovação em veículos elétricos?",
+        "alternativas": [
             "Mantendo a tecnologia em segredo",
             "Testando e desenvolvendo novas tecnologias",
             "Limitando o uso de novas tecnologias",
         ],
-        1,
-    ],
-    [
-        "Qual é uma diferença chave entre os pneus usados na Fórmula E e na Fórmula 1?",
-        [
+        "resposta_idx": 1,
+    },
+    {
+        "pergunta": "Qual é uma diferença chave entre os pneus usados na Fórmula E e na Fórmula 1?",
+        "alternativas": [
             "Os pneus da Fórmula E são recicláveis",
             "Os pneus da Fórmula E são maiores",
             "Os pneus da Fórmula E são menos duráveis",
         ],
-        0,
-    ],
-    [
-        "Como a Fórmula E influencia o mercado de veículos elétricos de consumo?",
-        [
+        "resposta_idx": 0,
+    },
+    {
+        "pergunta": "Como a Fórmula E influencia o mercado de veículos elétricos de consumo?",
+        "alternativas": [
             "Diminui o interesse em veículos elétricos",
             "Promove a aceitação e o desenvolvimento de veículos elétricos",
             "Não tem impacto no mercado de veículos elétricos",
         ],
-        1,
-    ],
+        "resposta_idx": 1,
+    },
 ]
+
 pontos = 0
 
 
@@ -117,34 +118,29 @@ def limpar_console():
 
 
 def embaralhar_lista(lista):
-    # https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
     # Utiliza o algoritmo Fisher–Yates para embaralhar a lista
     n = len(lista)
 
     for i in range(n):
-        # j <- inteiro aleatório que satisfaça a regra j >= i & j < n
         j = random.randint(i, n - 1)
-
-        # Atribuição múltipla, para não precisar de uma variável auxiliar
-        # https://stackoverflow.com/questions/51353078/multiple-attributions-per-line-in-python-versus-c
         lista[i], lista[j] = lista[j], lista[i]
-    return quiz
+    return lista
 
 
 embaralhar_lista(quiz)
 
-for pergunta, alternativas, resposta_idx in quiz:
+for pergunta in quiz:
     idx_resposta_usuario = exibir_pergunta_e_pegar_indice_resposta(
-        pergunta, alternativas, resposta_idx
+        pergunta["pergunta"], pergunta["alternativas"], pergunta["resposta_idx"]
     )
     limpar_console()
 
-    if idx_resposta_usuario == resposta_idx:
+    if idx_resposta_usuario == pergunta["resposta_idx"]:
         pontos += 1
         print("\033[32m[+] Você acertou a pergunta anterior :)\033[0m")
     else:
         print("\033[31m[=] Você errou a pergunta anterior :(\033[0m")
 
 print(
-    f"[!] Parabéns por concluir o Quiz! Você fez {pontos} pontos de {len(quiz)} possíveis"
+    f"[!] Parabéns por concluir o Quiz! Você fez {pontos} pontos de {len(quiz)} possíveis"
 )
